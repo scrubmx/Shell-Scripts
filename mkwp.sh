5e7d78e79cb01ac3382ed85da1ecbba2f274cf97
Jorge González <scrub.mx@gmail.com>
2013-10-22 13:28:58 -0500

    Update mkwp.sh

--- mkwp.sh
+++ mkwp.sh
@@ -1,33 +1,43 @@
 #! /bin/bash
-# Author: Jorge González
-# https://github.com/scrubmx/
+# Author: Jorge González (@scrubmx)
+# https://github.com/scrubmx/Shell-Scripts/edit/master/mkwp.sh
 #
 
 function _remove_wp_folders(){
 	mv wordpress/* ./
 	rmdir wordpress
+	rm *.tar.gz
 }
 
-function _install_spanish() {
-	wget http://es.wordpress.org/wordpress-3.6.1-es_ES.tar.gz
+function _install_wordpress(){
+	wget $1
 	printf '\033[0;34m%s\033[0m\n\n' "Extracting files and cleaning up..."
-	tar zxf wordpress-*.tar.gz
-	rm wordpress-*.tar.gz
+	tar xzf *.tar.gz
 	_remove_wp_folders
 }
 
-function _install_normal() {
-	wget http://wordpress.org/latest.tar.gz
-	printf '\033[0;34m%s\033[0m\n\n' "Extracting files and cleaning up..."
-	tar zxf latest.tar.gz
-	rm latest.tar.gz
-	_remove_wp_folders
+
+function _prompt_create_users(){
+	printf '\n\033[0;34m%s\033[0m' "Do you want to create default users? [Y/N] "
+	read users
+	if [ $users != Y ] || [ $users != y ]; then
+		rm -f ./wp-content/themes/base-theme/inc/users.php
+	fi
 }
 
 function _setup_base_theme(){
-	printf '\033[0;34m%s\033[0m\n\n' "Downloading HacemosCodigo base-theme..."
+	printf '\n\033[0;34m%s\033[0m\n\n' "Downloading HacemosCodigo base-theme..."
 	git clone git@github.com:HacemosCodigo/base-theme.git ./wp-content/themes/base-theme
 	rm -rf ./wp-content/themes/base-theme/.git
+	_prompt_create_users
+}
+
+function _prompt_base_theme(){
+	printf '\033[0;34m%s\033[0m' "Do you want to use the base-theme? [Y/N] "
+	read theme
+	if [ $theme = Y ] || [ $theme = y ]; then
+		_setup_base_theme
+	fi
 }
 
 # Select WordPress installation language.
@@ -38,12 +48,12 @@ printf '\n\033[0;34m%s\033[0m\n\n' "Downloading WordPress..."
 
 
 if [ $spanish = Y ] || [ $spanish = y ]; then
-	_install_spanish
-	_setup_base_theme
+	_install_wordpress "http://es.wordpress.org/wordpress-3.6.1-es_ES.tar.gz"
 else
-	_install_normal
-	_setup_base_theme
+	_install_wordpress "http://wordpress.org/latest.tar.gz"
 fi
+
+_prompt_base_theme
 
 printf '\n\033[0;34m%s\033[0m\n\n' "Enter your database information:"
 
